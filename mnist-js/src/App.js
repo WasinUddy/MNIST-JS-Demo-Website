5207ac0f77890b16c602f3ef953c824b12a74210
Wasin Silakong <38974240+WasinUddy@users.noreply.github.com>
2023-07-29 21:45:39 +0700

    App.js

--- mnist-js/src/App.js
+++ mnist-js/src/App.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 import CanvasGrid from './canvasGrid';
 import * as tf from '@tensorflow/tfjs';
-
-
+import { loadGraphModel } from '@tensorflow/tfjs';
 
 function App() {
-  // Create React.js Hooks to keep track of pixel drawed 28x28 grid of pixels 784 pixels
-  const [canvas, setCanvas] = useState(Array(28).fill(Array(28).fill(0)))
-  const [model, setModel] = useState(null)
+  const [canvas, setCanvas] = useState(Array(28).fill(Array(28).fill(0)));
+  const [model, setModel] = useState(null);
+  const [predictionResult, setPredictionResult] = useState(null);
 
-  // Load TensorFlow.js model
   useEffect(() => {
     async function loadModel() {
-      const loadedModel = await tf.loadLayersModel('assets/model.json')
-      setModel(loadedModel)
+      const loadedModel = await loadGraphModel('https://raw.githubusercontent.com/WasinUddy/MNIST-JS/main/tfjs_model/model.json');
+      setModel(loadedModel);
     }
+    loadModel();
+  }, []);
 
-    loadModel()
-  }, [])
-
-  // Function to convert canvas to tensor
   function preprocessImage(inputImage) {
-    // Convert canvas to batch dimensions [1, 28, 28, 1]
-    const tensor = tf.tensor2d(inputImage, [1, 28, 28, 1])
-
-    return tensor
+    const flatArray = inputImage.flatMap(row => row);
+    const tensor = tf.tensor4d(flatArray, [1, 28, 28, 1]);
+    return tensor;
   }
 
-  // Function to predict digit
   async function predictImage() {
     if (!model) {
-      console.error('Model not loaded')
-      return
+      console.error('Model not loaded');
+      return;
     }
 
-    const inputTensor = preprocessImage(canvas)
+    const inputTensor = preprocessImage(canvas);
+    const prediction = model.predict(inputTensor);
+    const result = Array.from(prediction.dataSync());
 
-    const prediction = model.predict(inputTensor)
+    // Find the index with the maximum value
+    const maxIndex = result.reduce((maxIndex, currValue, currIndex, arr) => currValue > arr[maxIndex] ? currIndex : maxIndex, 0);
 
-    const result = Array.from(prediction.dataSync())
+    setPredictionResult(maxIndex);
 
-    console.log(result)
-
-    inputTensor.dispose()
+    inputTensor.dispose();
   }
-  
+
   return (
     <div className="App">
-      <CanvasGrid canvas={canvas} setCanvas={setCanvas}/>
+      <CanvasGrid canvas={canvas} setCanvas={setCanvas} />
 
       <div className="button_container">
         <button className="clear_button" onClick={() => setCanvas(Array(28).fill(Array(28).fill(0)))}>Clear</button>
         <button className="predict_button" onClick={() => predictImage()}>Predict</button>
       </div>
+
+      {predictionResult !== null && (
+        <div className="prediction_display">
+          <p>Predicted Digit: {predictionResult}</p>
+        </div>
+      )}
     </div>
   );
 }
